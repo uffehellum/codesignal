b5699e5e0a8fe4e874514bed7526a014f2c882a1
uffe hellum <uffe.hellum@gmail.com>
2018-11-15 15:17:59 -0800

    medianscore

---
+++ medianscore/medianscore_test.go
@@ -0,0 +1,63 @@
+package medianscore
+
+import (
+	"container/heap"
+	"testing"
+)
+
+type ih []int
+
+func (h ih) Len() int            { return len(h) }
+func (h ih) Less(i, j int) bool  { return h[i] < h[j] }
+func (h ih) Swap(i, j int)       { h[i], h[j] = h[j], h[i] }
+func (h *ih) Push(x interface{}) { *h = append(*h, x.(int)) }
+
+func (h *ih) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+
+func medianScores(scores []int) (r []int) {
+	var low, high ih
+	heap.Init(&low)
+	heap.Init(&high)
+	for _, x := range scores {
+		heap.Push(&low, -x)
+		if len(low)-len(high) > 0 {
+			heap.Push(&high, -heap.Pop(&low).(int))
+		}
+		if len(low)-len(high) < 0 {
+			heap.Push(&low, -heap.Pop(&high).(int))
+		}
+		if len(low) > len(high) {
+			r = append(r, -low[0])
+		} else {
+			r = append(r, (high[0]-low[0])/2)
+		}
+	}
+	return
+}
+
+func test(t *testing.T, scores, expected []int) {
+	result := medianScores(scores)
+	if len(expected) != len(result) {
+		t.Error("wrong count. Got", result, "expected", expected)
+		return
+	}
+	for i := range expected {
+		if expected[i] != result[i] {
+			t.Error("wrong value pos", i, "got", result, "expected", expected)
+		}
+	}
+}
+
+func Test1(t *testing.T) {
+	test(t, []int{100, 20, 50, 70, 45}, []int{100, 60, 50, 60, 50})
+}
+
+func Test2(t *testing.T) {
+	test(t, []int{10, 20, 30}, []int{10, 15, 20})
+}
